d502e24294157ec47e018044544f8f4893462737
Juesgape <juesgape11@gmail.com>
2023-05-21 20:57:25 -0500

    Correcting some stuff

--- build/searcher.js
+++ build/searcher.js
@@ -1,36 +1,27 @@
 import Movies from "./movies.js";
-const searchActorButton = document.getElementById('searchButton');
+const searchButton = document.getElementById('searchButton');
 const searchContainer = document.getElementById('searchContainer');
-<<<<<<< HEAD
-const searchBar = document.querySelector('#searchContainer #searchInput');
-const searchButton = document.querySelector('#searchContainer #searchSubmit');
-searchActorButton.addEventListener('click', () => {
-    searchContainer.style.display = 'block';
-    searchButton.style.display = 'none';
-    searchBar.value = '';
-});
-searchButton.addEventListener('click', () => {
-    searchContainer.style.display = 'none';
-    searchButton.style.display = 'block';
-});
-searchButton.addEventListener('click', () => {
-    if (searchBar.value.length > 0) {
-        const actorName = searchBar.value;
-        Movies.getMoviesByActorName(actorName.trim());
-    }
-    else {
-        console.warn('Escribe algo memoochoa');
-    }
-=======
 const searchSubmit = document.getElementById('searchSubmit');
 const searchInput = document.getElementById('searchInput');
+//shows the searchbar
 searchButton.addEventListener('click', () => {
     searchContainer.style.display = 'block';
     searchButton.style.display = 'none';
     searchInput.value = '';
 });
+//Tells me what the user is typing
+/* searchBar.addEventListener('input', (e) => {
+  console.log(e.target.value)
+}) */
+//Search the actors
 searchSubmit.addEventListener('click', () => {
-    searchContainer.style.display = 'none';
-    searchButton.style.display = 'block';
->>>>>>> 144bf7d44997c3185e5e279219cbc8c6920b54d2
+    if (searchInput.value.length > 0) {
+        const actorName = searchInput.value;
+        Movies.getMoviesByActorName(actorName.trim());
+        searchInput.value = '';
+    }
+    else {
+        searchContainer.style.display = 'none';
+        searchButton.style.display = 'block';
+    }
 });
--- oop/searcher.ts
+++ oop/searcher.ts
@@ -1,4 +1,4 @@
-import Movies from "./movies";
+import Movies from "./movies.js";
 
 const searchButton = document.getElementById('searchButton') as HTMLButtonElement;
 const searchContainer = document.getElementById('searchContainer') as HTMLElement;
@@ -14,11 +14,6 @@ searchButton.addEventListener('click', () => {
 
 });
 
-searchSubmit.addEventListener('click', () => {
-  searchContainer.style.display = 'none';
-  searchButton.style.display = 'block';
-});
-
 //Tells me what the user is typing
 /* searchBar.addEventListener('input', (e) => {
   console.log(e.target.value)
@@ -26,12 +21,14 @@ searchSubmit.addEventListener('click', () => {
 
 //Search the actors
 searchSubmit.addEventListener('click', () => {
-  if(searchSubmit.value.length > 0) {
-    const actorName = searchSubmit.value
+  if(searchInput.value.length > 0) {
+    const actorName = searchInput.value
     Movies.getMoviesByActorName(actorName.trim())
+    searchInput.value = ''
 
   } else {
-    console.warn('Escribe algo memoochoa')
+    searchContainer.style.display = 'none';
+    searchButton.style.display = 'block';
   }
 
 })
